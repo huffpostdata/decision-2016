0d0fe2ec9dff65bea9256fcbc979ac42a16564f4
ASAP <alexander.sapountzis@gmail.com>
2016-10-14 17:43:03 -0400

    Add Senate and Hosue Seats

--- assets/javascripts/splash.js
+++ assets/javascripts/splash.js
@@ -1,8 +1,11 @@
 var battlegrounds = require('./splash/battlegrounds');
+var seats = require('./splash/seats');
 
 window.decision2016_init = function(data) {
   console.log("Sending data", data);
   battlegrounds.render(data.battlegrounds);
+  seats.renderHouse(data.house);
+  seats.renderSenate(data.senate);
 };
 
 // Warning: this script runs before DOMContentLoaded.
