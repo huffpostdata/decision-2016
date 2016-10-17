be46ac39100780e4a219b8ca98fdc2606439ba27
Adam Hooper <adam@adamhooper.com>
2016-10-17 17:58:53 -0400

    Update #president-summary dynamically

--- assets/javascripts/president.js
+++ assets/javascripts/president.js
@@ -1,6 +1,7 @@
 var cartogram = require('./common/_cartogram');
 var nav = require('./dashboard/_nav');
 var refresh = require('./common/_refresh');
+var summary = require('./president/_summary');
 
 var mapEl = document.getElementById('map');
 var cartogramEl = document.createElement('div');
@@ -11,9 +12,13 @@ var updateCartogram = cartogram(cartogramEl, { todo: 'put-data-here' });
 var navEl = document.querySelector('nav');
 var updateNav = nav(navEl);
 
+var summaryEl = document.getElementById('president-summary');
+var updateSummary = summary(summaryEl);
+
 function doRefresh(json) {
   updateCartogram(json.races);
   updateNav(json.summaries);
+  updateSummary(json);
 }
 
 var refreshEl = document.getElementById('refresh');
