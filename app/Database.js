6488a890a23204c8dd686e3d6e22c38be857c9bb
Jesse Kipp <jesse.kipp@huffingtonpost.com>
2016-10-25 16:05:31 -0400

    Correctly key translations by locale

--- app/Database.js
+++ app/Database.js
@@ -9,11 +9,23 @@ const PageMetadata = require('../generator/PageMetadata')
 const ap_fs = require('./ap/ap-fs')
 const presidentClassNameForRace = require('../assets/javascripts/president/_classNameForRace')
 
+function invertTranslations(translations) {
+  return translations.reduce(function(acc, phrase) {
+    const phraseId = phrase.id
+    Object.keys(phrase).forEach((locale) => {
+      if (locale === 'id' || locale === 'notes') { return acc }
+      if (typeof acc[locale] === 'undefined') { acc[locale] = {} }
+      acc[locale][phraseId] = phrase[locale]
+    })
+    return acc
+  }, {})
+}
+
 module.exports = class Database {
   constructor() {
     const google_docs = new GoogleDocs(read_config('google-docs'))
     const google_sheets = new GoogleSheets(read_config('google-sheets'))
-    const translations = google_sheets.slug_to_array('translations');
+    const translations = invertTranslations(google_sheets.slug_to_array('translations'));
     const apData = ap_fs.load()
 
     const battlegrounds = [
