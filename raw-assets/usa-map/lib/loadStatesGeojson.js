7d02c2d0141e10779f8005c1bc4785d29955448a
Adam Hooper <adam@adamhooper.com>
2016-10-25 19:48:35 -0400

    Missing file

---
+++ raw-assets/usa-map/lib/loadStatesGeojson.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const fs = require('fs')
+const debug = require('debug')('loadStatesGeojson')
+const defaultProjection = require('./defaultProjection')
+const makeGeojsonValid = require('./makeGeojsonValid')
+const quantizeAndMesh = require('./quantizeAndMesh')
+const projectGeojson = require('./projectGeojson')
+const shpjs = require('shpjs')
+
+function loadStatesGeojson() {
+  debug('Loading states')
+  const shp = fs.readFileSync(`${__dirname}/../input/statesp010g.shp`).buffer
+  const dbf = fs.readFileSync(`${__dirname}/../input/statesp010g.dbf`).buffer
+  const geojson = shpjs.combine([ shpjs.parseShp(shp), shpjs.parseDbf(dbf) ])
+  const validGeojson = makeGeojsonValid(geojson)
+  return validGeojson
+}
+
+function calculateStatesGeodata() {
+  const states = loadStatesGeojson()
+  debug('Filtering and projecting')
+  states.features = states.features
+    .filter(f => f.properties.TYPE === 'Land')
+    .filter(f => [ 'PR', 'GU', 'MP', 'VI', 'AS' ].indexOf(f.properties.STATE_ABBR) === -1)
+    .map(f => projectGeojson(f, defaultProjection))
+  return quantizeAndMesh(states)
+}
+
+module.exports = function() {
+  return calculateStatesGeodata()
+}
