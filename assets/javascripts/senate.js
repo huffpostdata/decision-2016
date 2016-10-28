d7b46f355c588a08ec18e92e3562396df8cbe334
Adam Hooper <adam@adamhooper.com>
2016-10-28 18:06:44 -0400

    Add Senate layout

--- assets/javascripts/senate.js
+++ assets/javascripts/senate.js
@@ -4,22 +4,22 @@ var refresh = require('./common/_refresh');
 
 var initialJson = JSON.parse(document.querySelector('script[data-json]').getAttribute('data-json'));
 
-var mapEl = document.getElementById('map');
-var mapSwitcherEl = document.getElementById('map-switcher');
-var map = new Map({
-  el: mapEl,
-  switchEl: mapSwitcherEl,
-  racesJson: initialJson.races
-});
+//var mapEl = document.getElementById('map');
+//var mapSwitcherEl = document.getElementById('map-switcher');
+//var map = new Map({
+//  el: mapEl,
+//  switchEl: mapSwitcherEl,
+//  racesJson: initialJson.races
+//});
 
 var navEl = document.querySelector('nav');
 var updateNav = nav(navEl);
 updateNav(initialJson.summaries);
 
 function doRefresh(json) {
-  map.update(json.races);
+  //map.update(json.races);
   updateNav(json.summaries);
 }
 
 var refreshEl = document.getElementById('refresh');
-refresh(refreshEl, '/2016/results/house.json', doRefresh);
+refresh(refreshEl, '/2016/results/senate.json', doRefresh);
