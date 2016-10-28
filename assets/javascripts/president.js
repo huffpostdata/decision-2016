777262fbcda406ca86eedc3261b28c5c447a8e90
Adam Hooper <adam@adamhooper.com>
2016-10-28 11:30:02 -0400

    Use dashboard/_map, not president/_map

--- assets/javascripts/president.js
+++ assets/javascripts/president.js
@@ -1,21 +1,30 @@
-var Map = require('./president/_map');
+var Map = require('./dashboard/_map');
 var nav = require('./dashboard/_nav');
 var refresh = require('./common/_refresh');
 var summary = require('./president/_summary');
 var tooltip = require('./president/_tooltip');
 
+var initialJson = JSON.parse(document.querySelector('script[data-json]').getAttribute('data-json'));
+
 var mapEl = document.getElementById('map');
 var mapSwitcherEl = document.getElementById('map-switcher');
-var map = new Map(mapEl, mapSwitcherEl);
+var map = new Map({
+  el: mapEl,
+  switchEl: mapSwitcherEl,
+  racesJson: initialJson.races
+});
 
 var navEl = document.querySelector('nav');
 var updateNav = nav(navEl);
+updateNav(initialJson.summaries);
 
 var summaryEl = document.getElementById('president-summary');
 var updateSummary = summary(summaryEl);
+updateSummary(initialJson);
 
 var tooltipEl = document.getElementById('tooltip');
 var toolTip = tooltip(tooltipEl);
+toolTip.setData(initialJson.races);
 
 function doRefresh(json) {
   map.update(json.races);
@@ -26,6 +35,3 @@ function doRefresh(json) {
 
 var refreshEl = document.getElementById('refresh');
 refresh(refreshEl, '/2016/results/president.json', doRefresh);
-
-var initialJson = document.querySelector('script[data-json]').getAttribute('data-json');
-doRefresh(JSON.parse(initialJson));
