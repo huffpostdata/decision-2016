2c1cb891f2a10af33c5b4983540a6e781dcd54de
Adam Hooper <adam@adamhooper.com>
2016-11-10 12:53:59 -0500

    Use relative paths in generator

--- generator/read_config.js
+++ generator/read_config.js
@@ -2,5 +2,5 @@ const fs = require('fs')
 const yaml = require('js-yaml')
 
 module.exports = function read_config(basename) {
-  return yaml.safeLoad(fs.readFileSync(`config/${basename}.yml`, 'utf-8'))
+  return yaml.safeLoad(fs.readFileSync(`${__dirname}/../config/${basename}.yml`, 'utf-8'))
 }
